7cb336bf3d64269e3ffb2dc8ff7c3a73db5a7300
Wratheus <morwar211@gmail.com>
2021-09-23 16:58:24 +0300

    added Score class and getUserScore() request

---
+++ osu_track_app/lib/src/scores.dart
@@ -0,0 +1,28 @@
+// Score includes beatmap class to avoid beatmap request
+class Score{
+  int?scoreId;
+  List<dynamic> mods = [];
+  int?scorePoints;
+  int?maxCombo;
+  List<dynamic>?mapStatistics;
+  String?mapRank;
+  String?dateOfScore;
+  int?bestScoreOnMapId;
+  double?gainedPP;
+  int?beatmapSetId;
+  int?beatmapSetMapId;
+  double?difficultyRating;
+  int?beatmapLength;
+  String?defficultyName;
+  String?OD;
+  String?AR;
+  String?BPM;
+  String?CS;
+  String?HP;
+  String?beatmapURL;
+  String?artistName;
+  List<String>?coversJPG;
+  String?mapTittle;
+  String?mapperName;
+  String?mapperId;
+}
