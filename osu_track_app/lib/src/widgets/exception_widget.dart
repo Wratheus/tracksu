79f8836fc6c712553eb161cc3fc927dc20072a36
Wratheus <morwar211@gmail.com>
2021-10-12 22:56:38 +0300

    style: add iconpack, logos, change icon of navBar

---
+++ osu_track_app/lib/src/widgets/exception_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExceptionWidget extends StatelessWidget {
+  const ExceptionWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+//soon
